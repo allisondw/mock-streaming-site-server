00f69e55f7dec939d5eb2b2a8b65e577e4f09221
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-01-12 14:20:16 -0800

    Add dotenv and static files

--- routes/videos.js
+++ routes/videos.js
@@ -1,26 +1,65 @@
 const express = require("express");
 const router = express.Router();
-const { v4: uuidv4 } = require("uuid"); 
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
-const videoData = require("../data/video-details.json");
 
-const getVideosList = () => {
-    let videosList = videoData.map(video => {
-        return {
-            id: video.id,
-            title: video.title,
-            channel: video.channel,
-            image: video.image
-        }
-    })
-    return videosList;
-}
-
-router.get('/', (req, res) => {
+const readVideosData = () => {
     try {
-        if(videoData) {
+        const videosData = fs.readFileSync("./data/video-details.json", "utf8");
+        const videos = JSON.parse(videosData);
+        return videos;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-        }
+const writeVideosData = (data) => {
+  const stringifiedVideo = JSON.stringify(data);
+  fs.writeFileSync('./data/video-details.json', stringifiedVideo);
+};
+
+router.route("/").get((req, res) => {
+    console.log("working");
+  const videos = readVideosData();
+  const bareVideo = videos.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image
+    };
+  });
+  res.status(200).json(bareVideo);
+}).post((req, res) => {
+    const videos = readVideosData();
+    const newVideo = {
+        id: uuidv4(),
+        title: req.body.title,
+        channel: "Private Channel",
+        image: "../public/upload-preview.jpg",
+        description: req.body.description,
+        views: 0,
+        likes: 0,
+        duration: "10:22",
+        video: '',
+        timestamp: Date.now(),
+        comments: []
+    }
+    videos.push(newVideo);
+    writeVideosData(videos);
+    res.status(200).send("Video successfully uploaded!");
+});
+
+router.get('/:videoId', (req, res) => {
+    const videos = readVideosData();
+    const videoId = req.params.videoId;
+    const selectedVideo = videos.find(video => video.id === videoId);
+    if(selectedVideo) {
+        res.json(selectedVideo);
+    } else {
+        res.status(404).send("Video not found");
     }
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-import express from 'express';
-
+const express = require('express');
+const videosRoutes = require("./routes/videos.js");
+const cors = require('cors');
+require("dotenv").config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.get('/videos', (req, res) => {
-    res.send("This will be the route for fetching all videos");
-});
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
 
-app.get('/videos/:videoId', (req, res) => {
-    res.send("This will be the route for fetcing selected video");
-});
-
-app.post('/videos', (req, res) => {
-    res.send("This will be the route for posting a new video")
-})
+app.use('/videos', videosRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+
+
+
+
+// app.get('/videos', (req, res) => {
+//     res.send("This will be the route for fetching all videos");
+// });
+
+// app.get('/videos/:videoId', (req, res) => {
+//     res.send("This will be the route for fetcing selected video");
+// });
+
+// app.post('/videos', (req, res) => {
+//     res.send("This will be the route for posting a new video")
+// })
